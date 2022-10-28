344d33ded33822e7015e59fffcb954a031f7d7df
Aung-Thu-Zaw <aungthuzaw223@gmail.com>
2022-10-28 11:18:38 +0630

    output result card styling

--- app.vue
+++ app.vue
@@ -76,6 +76,15 @@
       </div>
       <button class="primary" @click="computeSelectedNames">Find Names</button>
     </div>
+
+    <div class="card-container">
+      <div class="card" v-for="name in selectedNames" :key="name">
+        <h4>
+          {{ name }}
+        </h4>
+        <p>x</p>
+      </div>
+    </div>
   </div>
 
   <!-- <div>
@@ -184,5 +193,28 @@ console.log("this is a selected names:", selectedNames);
   font-size: 1rem;
   margin-top: 1rem;
   cursor: pointer;
+}
+
+.card-container {
+  display: flex;
+  margin-top: 3rem;
+  flex-wrap: wrap;
+}
+.card {
+  background-color: rgb(27, 60, 138);
+  width: 28%;
+  color: white;
+  border-radius: 1rem;
+  padding: 0.1rem;
+  margin-right: 0.5rem;
+  margin-bottom: 1rem;
+  position: relative;
+}
+.card p {
+  position: absolute;
+  top: -15%;
+  left: 90%;
+  cursor: pointer;
+  color: rgba(255, 255, 255, 0.178);
 }
 </style>
